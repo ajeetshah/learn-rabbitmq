acd3b5a3eb1d5751cabc7ac7d0ffb04b15008f20
Ajeet <ajeets.shah.87@gmail.com>
2025-02-23 01:33:17 +0530

    git init

---
+++ log-aggregator/src/main/java/com/example/log_aggregator/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.log_aggregator.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+  @GetMapping("/v1/world")
+  public String world() {
+    return "world";
+  }
+
+}
---
+++ service-three/src/main/java/com/example/service_three/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.service_three.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+  @GetMapping("/v1/world")
+  public String world() {
+    return "world";
+  }
+
+}
